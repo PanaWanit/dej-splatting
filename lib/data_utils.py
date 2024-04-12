65af32d61c42fd2f4ddf8b28bf10a5206235f159
nonewanit <none.wanit@gmail.com>
2024-04-12 16:40:05 +0700

    Add read images

--- lib/data_utils.py
+++ lib/data_utils.py
@@ -4,4 +4,10 @@ import numpy as np
 import torch
 from torch import nn
 
-
+# Read images.txt
+def read_images(images_file_path : str):
+    with open(images_file_path) as f:
+        images_list = [line.split() for line in f.readlines()[::2]]
+        images_columns = ['IMAGE_ID', 'QW', 'QX', 'QY', 'QZ', 'TX', 'TY', 'TZ', 'CAMERA_ID', 'NAME']
+        images_df = pd.DataFrame(images_list, columns = images_columns, dtype = float)
+    return images_df
