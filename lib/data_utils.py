2d0090340d3a77a5a89f574d864066d3e1271128
Pana Wanitchollakit <pnaw.wan@gmail.com>
2024-04-12 16:38:41 +0700

    read_camera

--- lib/data_utils.py
+++ lib/data_utils.py
@@ -4,4 +4,19 @@ import numpy as np
 import torch
 from torch import nn
 
-
+def read_cameras(camera_file_path:str) -> pd.DataFrame:
+    columns = ['CAM_ID', 'MODEL', 'W', 'H', 'FocalX', 'FocalY', 'PrincX', 'PrincY']
+    columns_type = {
+        'CAM_ID': 'int',
+        'MODEL': 'str',
+        'W': 'int',
+        'H': 'int',
+        'FocalX': 'float',
+        'FocalY': 'float',
+        'PrincX': 'float',
+        'PrincY': 'float',
+    }
+    with open(camera_file_path, 'r') as f:
+        cam_lists = [x.split() for x in f.readlines()]
+    df = pd.DataFrame(cam_lists, columns=columns).astype(columns_type)
+    return df
