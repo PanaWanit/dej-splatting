4365672c913dda8c76b49f8a22c86a9e29fd0a24
Pana Wanitchollakit <pnaw.wan@gmail.com>
2024-04-12 16:51:54 +0700

    format read cam

--- lib/data_utils.py
+++ lib/data_utils.py
@@ -27,14 +27,14 @@ def read_images(images_file_path : str):
 def read_cameras(camera_file_path:str) -> pd.DataFrame:
     columns = ['CAM_ID', 'MODEL', 'W', 'H', 'FocalX', 'FocalY', 'PrincX', 'PrincY']
     columns_type = {
-        'CAM_ID': 'int',
-        'MODEL': 'str',
-        'W': 'int',
-        'H': 'int',
-        'FocalX': 'float',
-        'FocalY': 'float',
-        'PrincX': 'float',
-        'PrincY': 'float',
+        'CAM_ID': int,
+        'MODEL': str,
+        'W': int,
+        'H': int,
+        'FocalX': float,
+        'FocalY': float,
+        'PrincX': float,
+        'PrincY': float,
     }
     with open(camera_file_path, 'r') as f:
         cam_lists = [x.split() for x in f.readlines()]
