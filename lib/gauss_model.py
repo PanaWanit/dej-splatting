920e610296409bba813dfe7c78dca44345848818
Snowfall2 <atittarn2@gmail.com>
2024-04-12 16:07:12 +0700

    Modify gauss_model file

---
+++ lib/gauss_model.py
@@ -0,0 +1,133 @@
+import torch
+import torch.nn as nn
+import numpy as np
+from gauss_utils import *
+from point_utils import PointCloud
+from sh_utils import RGB2SH
+from plyfile import PlyData, PlyElement
+
+class GaussModel(nn.Module):
+    """
+    Gaussian Model
+
+    Parameters
+    _xyz = position of gaussian
+    _feature_dc: DC term of features
+    _feature_rest: rest features
+    _rotation: rotation of gaussians (quarternion)
+    _scaling: scaling of gaussians in each axis
+    _opacity: opacity of gaussians (alpha)
+
+    """
+    def __init__(self, sh_degree: int=3):
+        super(GaussModel, self).__init__()
+        self.sh_degree = sh_degree
+        self._xyz = torch.empty(0)
+        self._features_dc = torch.empty(0)
+        self._features_rest = torch.empty(0)
+        self._scaling = torch.empty(0)
+        self._rotation = torch.empty(0)
+        self._opacity = torch.empty(0)
+        self.setup(self)
+    
+    def setup(self):
+        def init_covariance_from_scaling_rotation(scaling, scaling_modifier, rotation):
+            L = init_scaling_rotation(scaling_modifier * scaling, rotation)
+            actual_covariance = L @ L.transpose(1, 2)
+            # Get lower diagonal elements from 3*3 matrix because it's symmetric
+            sym = get_lowerdiag(actual_covariance)
+            return sym
+        
+        self.scaling_activation = torch.exp
+        self.scaling_inverse_activation = torch.log
+        self.covariance_activation = init_covariance_from_scaling_rotation
+
+        self.opacity_activation = torch.sigmoid
+        self.inverse_opacity_activation = inverse_sigmoid
+
+        self.rotation_activation = torch.nn.functional.normalize
+    
+    def create_from_pcd(self, pcd:PointCloud):
+        """
+            create the guassian model from a color point cloud
+        """
+        points = pcd.coords
+        colors = pcd.select_channels(['R', 'G', 'B']) / 255.
+
+        fused_point_cloud = torch.tensor(np.asarray(points)).float().cuda()
+        fused_color = RGB2SH(torch.tensor(np.asarray(colors)).float().cuda())
+
+        print("Initialize point : ", fused_point_cloud.shape[0])
+
+        features = torch.zeros((fused_color.shape[0], 3, (self.max_sh_degree + 1) ** 2)).float().cuda()
+        features[:, :3, 0 ] = fused_color
+        features[:, 3:, 1:] = 0.0
+
+        dist2 = torch.clamp_min(distCUDA2(torch.from_numpy(np.asarray(points)).float().cuda()), 0.0000001)
+        scales = torch.log(torch.sqrt(dist2))[...,None].repeat(1, 3)
+        rots = torch.zeros((fused_point_cloud.shape[0], 4), device="cuda")
+        rots[:, 0] = 1
+        opacities = inverse_sigmoid(0.1 * torch.ones((fused_point_cloud.shape[0], 1), dtype=torch.float, device="cuda"))
+
+        self._xyz = nn.Parameter(fused_point_cloud.requires_grad_(True))
+        self._features_dc = nn.Parameter(features[:,:,0:1].transpose(1, 2).contiguous().requires_grad_(True))
+        self._features_rest = nn.Parameter(features[:,:,1:].transpose(1, 2).contiguous().requires_grad_(True))
+        self._scaling = nn.Parameter(scales.requires_grad_(True))
+        self._rotation = nn.Parameter(rots.requires_grad_(True))
+        self._opacity = nn.Parameter(opacities.requires_grad_(True))
+        self.max_radii2D = torch.zeros((self._xyz.shape[0]), device="cuda")
+        return self
+    
+    @property
+    def get_scaling(self):
+        return self.scaling_activation(self._scaling)
+    
+    @property
+    def get_rotation(self):
+        return self.rotation_activation(self._rotation)
+    
+    @property
+    def get_xyz(self):
+        return self._xyz
+    
+    @property
+    def get_features(self):
+        return torch.cat((self._features_dc, self._features_rest), dim=1)
+    
+    @property
+    def get_opacity(self):
+        return self.opacity_activation(self._opacity)
+    
+    def get_covariance(self, scaling_modifier = 1):
+        return self.covariance_activation(self.get_scaling, scaling_modifier, self._rotation)
+
+    def construct_list_of_attributes(self):
+        l = ['x', 'y', 'z', 'nx', 'ny', 'nz']
+        # All channels except the 3 DC
+        for i in range(self._features_dc.shape[1]*self._features_dc.shape[2]):
+            l.append('f_dc_{}'.format(i))
+        for i in range(self._features_rest.shape[1]*self._features_rest.shape[2]):
+            l.append('f_rest_{}'.format(i))
+        l.append('opacity')
+        for i in range(self._scaling.shape[1]):
+            l.append('scale_{}'.format(i))
+        for i in range(self._rotation.shape[1]):
+            l.append('rot_{}'.format(i))
+        return l
+    
+    def save_ply(self, path):
+        xyz = self._xyz.detach().cpu().numpy()
+        normals = np.zeros_like(xyz)
+        features_dc = self._features_dc.detach().transpose(1, 2).flatten(start_dim=1).contiguous().cpu().numpy()
+        features_rest = self._features_rest.detach().transpose(1, 2).flatten(start_dim=1).contiguous().cpu().numpy()
+        opacities = self._opacity.detach().cpu().numpy()
+        scale = self._scaling.detach().cpu().numpy()
+        rotation = self._rotation.detach().cpu().numpy()
+
+        # Change data type of all attribute to float32
+        dtypes = [(attribute, 'f4') for attribute in self.construct_list_of_attributes()]
+
+        elements = np.empty(xyz.shape[0], dtype=dtypes)
+        attributes = np.concatenate((xyz, normals, features_dc, features_rest, opacities, scale, rotation), axis=1)
+        elements[:] = list(map(tuple, attributes))
+        PlyData([PlyElement.describe(elements, 'vertex')]).write(path)
