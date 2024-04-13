8e25c323b6802db0fa63a97824f3b092274011dd
Pana Wanitchollakit <pnaw.wan@gmail.com>
2024-04-13 11:52:23 +0700

    Add Gaussisan Trainer

--- lib/data_utils.py
+++ lib/data_utils.py
@@ -4,40 +4,41 @@ import numpy as np
 import torch
 import torch.functional as F
 from torch import nn
-from transformcov import gen_rotation
+from lib.transformcov import gen_rotation
 import imageio
 
-# Read images.txt
-def read_images_colmap(images_file_path : str, scale_factor:float = 1):
 
-    images_columns = ['IMAGE_ID', 'QW', 'QX', 'QY', 'QZ', 'TX', 'TY', 'TZ', 'CAMERA_ID', 'NAME']
+# Read images.txt
+def read_images_colmap(images_file_path: str, scale_factor: float = 1):
+
+    images_columns = ["IMAGE_ID", "QW", "QX", "QY", "QZ", "TX", "TY", "TZ", "CAMERA_ID", "NAME"]
     columns_type = {
-        'IMAGE_ID': int,
-        'QW': float,
-        'QX': float,
-        'QY': float,
-        'QZ': float,
-        'TX': float,
-        'TY': float,
-        'TZ': float,
-        'CAMERA_ID': int,
-        'NAME': str,
+        "IMAGE_ID": int,
+        "QW": float,
+        "QX": float,
+        "QY": float,
+        "QZ": float,
+        "TX": float,
+        "TY": float,
+        "TZ": float,
+        "CAMERA_ID": int,
+        "NAME": str,
     }
 
     with open(images_file_path) as f:
         images_list = [line.split() for line in f.readlines()[::2]]
 
-    df = pd.DataFrame(images_list, columns = images_columns).astype(columns_type)
-    
+    df = pd.DataFrame(images_list, columns=images_columns).astype(columns_type)
+
     # Compute extrinsic
-    rot = torch.from_numpy(df[['QW', 'QX', 'QY', 'QZ']].to_numpy())
+    rot = torch.from_numpy(df[["QW", "QX", "QY", "QZ"]].to_numpy())
     R = gen_rotation(rot)
     extrinsics = torch.empty(R.size(0), 3, 4)
 
     extrinsics[:, :3, :3] = R
-    extrinsics[:, 0, 3] = torch.from_numpy(df['TX'].to_numpy())
-    extrinsics[:, 1, 3] = torch.from_numpy(df['TY'].to_numpy())
-    extrinsics[:, 2, 3] = torch.from_numpy(df['TZ'].to_numpy())
+    extrinsics[:, 0, 3] = torch.from_numpy(df["TX"].to_numpy())
+    extrinsics[:, 1, 3] = torch.from_numpy(df["TY"].to_numpy())
+    extrinsics[:, 2, 3] = torch.from_numpy(df["TZ"].to_numpy())
 
     # Read image rgb with down sampling
     def read_image(image_path, scale_factor):
@@ -50,46 +51,49 @@ def read_images_colmap(images_file_path : str, scale_factor:float = 1):
             image_size[0] = image_size[0] * scale_factor
             image_size[0] = image_size[1] * scale_factor
             image = image.unsqueeze(0)
-            image = F.interpolate(image.permute(0, 3, 1, 2), scale_factor=scale_factor, mode='bilinear').permute(0, 2, 3, 1) # [B, C, H, W]
+            image = F.interpolate(image.permute(0, 3, 1, 2), scale_factor=scale_factor, mode="bilinear").permute(
+                0, 2, 3, 1
+            )  # [B, C, H, W]
             image = image.squeeze(0)
 
         return image
 
+    rgbs = [read_image(img_path, scale_factor) for img_path in df["NAME"]]
 
-    rgbs = [read_image(img_path, scale_factor) for img_path in df['NAME']]
-        
-    return extrinsics, R, df['CAMERA_ID'].to_numpy(), rgbs, df
+    return extrinsics, R, df["CAMERA_ID"].to_numpy(), rgbs, df
 
-def read_cameras_colmap(camera_file_path:str):
-    columns = ['CAM_ID', 'MODEL', 'W', 'H', 'FocalX', 'FocalY', 'PrincX', 'PrincY']
+
+def read_cameras_colmap(camera_file_path: str):
+    columns = ["CAM_ID", "MODEL", "W", "H", "FocalX", "FocalY", "PrincX", "PrincY"]
     columns_type = {
-        'CAM_ID': int,
-        'MODEL': str,
-        'W': int,
-        'H': int,
-        'FocalX': float,
-        'FocalY': float,
-        'PrincX': float,
-        'PrincY': float,
+        "CAM_ID": int,
+        "MODEL": str,
+        "W": int,
+        "H": int,
+        "FocalX": float,
+        "FocalY": float,
+        "PrincX": float,
+        "PrincY": float,
     }
-    with open(camera_file_path, 'r') as f:
+    with open(camera_file_path, "r") as f:
         cam_lists = [x.split() for x in f.readlines()]
     df = pd.DataFrame(cam_lists, columns=columns).astype(columns_type)
 
     intrinsics = torch.zeros(df.shape[0], 3, 3)
-    intrinsics[:, 0, 0] = torch.from_numpy(df['FocalX'].to_numpy())
-    intrinsics[:, 1, 1] = torch.from_numpy(df['FocalY'].to_numpy())
-    intrinsics[:, 0, 2] = torch.from_numpy(df['PrincX'].to_numpy())
-    intrinsics[:, 1, 2] = torch.from_numpy(df['PrincY'].to_numpy())
+    intrinsics[:, 0, 0] = torch.from_numpy(df["FocalX"].to_numpy())
+    intrinsics[:, 1, 1] = torch.from_numpy(df["FocalY"].to_numpy())
+    intrinsics[:, 0, 2] = torch.from_numpy(df["PrincX"].to_numpy())
+    intrinsics[:, 1, 2] = torch.from_numpy(df["PrincY"].to_numpy())
     intrinsics[:, 2, 2] = 1
-    return intrinsics, torch.from_numpy(df['W'].to_numpy()), torch.from_numpy(df['H'].to_numpy()), df
+    return intrinsics, torch.from_numpy(df["W"].to_numpy()), torch.from_numpy(df["H"].to_numpy()), df
 
-def read_all(images_file_path:str, camera_file_path:str):
+
+def read_all(images_file_path: str, camera_file_path: str):
     extrinsics, R, cam_ids, rgbs, image_df = read_images_colmap(images_file_path)
     intrinsics, Ws, Hs, camera_df = read_cameras_colmap(camera_file_path)
 
     properties = []
-    
+
     for idx, cam_id in enumerate(cam_ids):
         cur_intrinsic = intrinsics[cam_id]
         cur_extrinsic = extrinsics[idx]
@@ -100,16 +104,13 @@ def read_all(images_file_path:str, camera_file_path:str):
         cur_image_df = image_df.iloc[idx]
         cur_camera_df = camera_df.iloc[cam_id]
 
-        w2img =  cur_intrinsic @ cur_extrinsic
+        w2img = cur_intrinsic @ cur_extrinsic
 
-        properties.append({
-            'rgb': cur_rgb,
-            'R': cur_R,
-            'w2img': w2img,
-            'image_df': cur_image_df,
-            'camera_df': cur_camera_df
-        })
+        properties.append(
+            {"rgb": cur_rgb, "R": cur_R, "w2img": w2img, "image_df": cur_image_df, "camera_df": cur_camera_df}
+        )
     return properties
-        
-if __name__ == '__main__':
-    print(read_all('./images.txt', 'test_cam.txt'))
+
+
+if __name__ == "__main__":
+    print(read_all("./images.txt", "test_cam.txt"))
---
+++ lib/gaussTrainer.py
@@ -0,0 +1,210 @@
+import os
+from accelerate import Accelerator
+from torch.optim import Adam
+from lib.data_utils import read_all
+from pathlib import Path
+import torch
+import numpy as np
+from tqdm import tqdm
+
+import contextlib
+
+USE_GPU_PYTORCH = True
+USE_PROFILE = False
+
+
+def exists(x):
+    return x is not None
+
+
+class GaussTrainer:
+    def __init__(self, model, config, **kwargs):
+        super().__init__()
+
+        self.data = read_all(
+            os.path.join(config.database_path, "image.txt"),
+            os.path.join(config.database_path, "camera.txt"),
+        )
+
+        ##### TODO: Implement gauss renderer
+        self.gaussRender = GaussRenderer(**kwargs.get("render_kwargs", {}))
+        ###
+
+        self.lambda_dssim = config.lambda_dssim
+        self.lambda_depth = config.lambda_depth
+
+        self.accelerator = Accelerator(
+            split_batches=config.split_batches,
+            mixed_precision="fp16" if config.fp16 else "no",
+            project_dir=config.results_folder if config.with_tracking else None,
+            log_with="all",
+        )
+
+        self.accelerator.native_amp = config.amp
+
+        self.model = model
+
+        self.train_lr = config.lr
+        self.train_batch_size = config.batch_size
+        self.train_num_steps = config.epochs
+
+        self.i_save = config.i_save
+        self.i_print = config.i_print
+        self.i_image = config.i_image
+
+        self.results_folder = config.results_folder
+        self.gradient_accumulate_every = config.gradient_accumulate_every
+        self.with_tracking = config.with_tracking
+        self.step = 0
+
+        self.opt = Adam(self.model.parameters(), lr=config.train_lr, betas=config.adam_betas)
+
+        if self.accelerator.is_main_process:
+            self.results_folder = Path(config.results_folder)
+            self.results_folder.mkdir(exist_ok=True)
+
+        self.model, self.opt = self.accelerator.prepare(self.model, self.opt)
+
+        # tracking
+        if self.with_tracking:
+            run = os.path.split(__file__)[-1].split(".")[0]
+            self.accelerator.init_trackers(
+                run,
+                config={
+                    "train_lr": config.train_lr,
+                    "train_batch_size": config.train_batch_size,
+                    "gradient_accumulate_every": config.gradient_accumulate_every,
+                    "train_num_steps": config.train_num_steps,
+                },
+            )
+
+    def save(self, milestone):
+        if not self.accelerator.is_local_main_process:
+            return
+
+        data = {
+            "step": self.step,
+            "model": self.accelerator.get_state_dict(self.model),
+            "opt": self.opt.state_dict(),
+            "scaler": self.accelerator.scaler.state_dict() if exists(self.accelerator.scaler) else None,
+        }
+
+        torch.save(data, str(self.results_folder / f"model-{milestone}.pt"))
+
+    def load(self, milestone):
+        if not self.accelerator.is_local_main_process:
+            return
+
+        accelerator = self.accelerator
+        device = accelerator.device
+
+        data = torch.load(str(self.results_folder / f"model-{milestone}.pt"), map_location=device)
+
+        model = self.accelerator.unwrap_model(self.model)
+        model.load_state_dict(data["model"])
+
+        self.step = data["step"]
+        self.opt.load_state_dict(data["opt"])
+
+        if exists(self.accelerator.scaler) and exists(data["scaler"]):
+            self.accelerator.scaler.load_state_dict(data["scaler"])
+
+    def on_train_step(self):
+        # TODO: adapt to index
+        ind = np.random.choice(len(self.data["camera"]))
+        camera = self.data["camera"][ind]
+        rgb = self.data["rgb"][ind]
+        depth = self.data["depth"][ind]
+        mask = self.data["alpha"][ind] > 0.5
+        # TODO: Implement camera util
+        # NOTE: to_view_point pass data instead
+        if USE_GPU_PYTORCH:
+            camera = to_viewpoint_camera(self.data[ind])
+
+        if USE_PROFILE:
+            prof = profile(activities=[ProfilerActivity.CUDA], with_stack=True)
+        else:
+            prof = contextlib.nullcontext()
+
+        with prof:
+            out = self.gaussRender(pc=self.model, camera=camera)
+
+        if USE_PROFILE:
+            print(prof.key_averages(group_by_stack_n=True).table(sort_by="self_cuda_time_total", row_limit=20))
+        # TODO: Implement loss utils
+        l1_loss = loss_utils.l1_loss(out["render"], rgb)
+        depth_loss = loss_utils.l1_loss(out["depth"][..., 0][mask], depth[mask])
+        ssim_loss = 1.0 - loss_utils.ssim(out["render"], rgb)
+
+        total_loss = (1 - self.lambda_dssim) * l1_loss + self.lambda_dssim * ssim_loss + depth_loss * self.lambda_depth
+        psnr = utils.img2psnr(out["render"], rgb)
+        log_dict = {"total": total_loss, "l1": l1_loss, "ssim": ssim_loss, "depth": depth_loss, "psnr": psnr}
+
+        return total_loss, log_dict
+
+    def on_evaluate_step(self, **kwargs):
+        import matplotlib.pyplot as plt
+
+        ind = np.random.choice(len(self.data["camera"]))
+        camera = self.data["camera"][ind]
+        if USE_GPU_PYTORCH:
+            camera = to_viewpoint_camera(camera)
+
+        rgb = self.data["rgb"][ind].detach().cpu().numpy()
+        out = self.gaussRender(pc=self.model, camera=camera)
+        rgb_pd = out["render"].detach().cpu().numpy()
+        depth_pd = out["depth"].detach().cpu().numpy()[..., 0]
+        depth = self.data["depth"][ind].detach().cpu().numpy()
+        depth = np.concatenate([depth, depth_pd], axis=1)
+        depth = 1 - depth / depth.max()
+        depth = plt.get_cmap("jet")(depth)[..., :3]
+        image = np.concatenate([rgb, rgb_pd], axis=1)
+        image = np.concatenate([image, depth], axis=0)
+        utils.imwrite(str(self.results_folder / f"image-{self.step}.png"), image)
+
+    def train(self):
+        accelerator = self.accelerator
+        device = accelerator.device
+
+        with tqdm(initial=self.step, total=self.train_num_steps, disable=not accelerator.is_main_process) as pbar:
+
+            while self.step < self.train_num_steps:
+
+                total_loss = 0.0
+
+                for _ in range(self.gradient_accumulate_every):
+
+                    with self.accelerator.autocast():
+                        loss, log_dict = self.on_train_step()
+                        loss = loss / self.gradient_accumulate_every
+                        total_loss += loss
+
+                    self.accelerator.backward(loss)
+
+                # all reduce to get the total loss
+                total_loss = accelerator.reduce(total_loss)
+                total_loss = total_loss.item()
+                log_str = f"loss: {total_loss:.3f}"
+
+                for k in log_dict.keys():
+                    log_str += " {}: {:.3f}".format(k, log_dict[k])
+
+                pbar.set_description(log_str)
+
+                self.opt.step()
+                self.opt.zero_grad()
+
+                self.step += 1
+                if accelerator.is_main_process:
+
+                    if self.step % self.i_image == 0:
+                        self.on_evaluate_step()
+
+                    if self.step != 0 and (self.step % self.i_save == 0):
+                        milestone = self.step // self.i_save
+                        self.save(milestone)
+
+                pbar.update(1)
+
+        if self.with_tracking:
+            accelerator.end_training()
